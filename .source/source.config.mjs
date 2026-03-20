646b9342fc90c3ca121dfb1db9c0a64931eb051a
lioneltchami <lioneltchami@gmail.com>
2026-03-20 14:12:04 -0600

    other

--- .source/source.config.mjs
+++ .source/source.config.mjs
@@ -1,9 +1,14 @@
 // source.config.ts
-import { defineDocs, defineConfig } from "fumadocs-mdx/config";
+import { rehypeCode } from "fumadocs-core/mdx-plugins";
+import { defineConfig, defineDocs } from "fumadocs-mdx/config";
 var { docs, meta } = defineDocs({
   dir: "content/docs"
 });
-var source_config_default = defineConfig();
+var source_config_default = defineConfig({
+  mdxOptions: {
+    rehypePlugins: [rehypeCode]
+  }
+});
 export {
   source_config_default as default,
   docs,
